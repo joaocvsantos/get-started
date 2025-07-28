f83325fb007e729e7e8a383b5befc1a53674226a
joaocvsantos <73187460+joaocvsantos@users.noreply.github.com>
2025-07-28 19:06:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app, version 2")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+value = st.slider('Select a value', min_value=0, max_value=100, value=50)
